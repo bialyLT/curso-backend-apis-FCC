fbc397c19ef3584a7bf9bb5e422e9c110266d6c5
liam <liambialy2@gmail.com>
2024-02-11 04:14:31 -0300

    section-2 finished

--- myApp.js
+++ myApp.js
@@ -1,38 +1,14 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser')
 
+app.use((bodyParser.urlencoded({extended: false})))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post('/name', (req, res) => {
+    res.json({
+        name: `${req.body.first} ${req.body.last}`
+    })
+} )
 
 
 
