362677d7b025cd834ef832fe10c9694c799ee433
liam <liambialy2@gmail.com>
2024-02-12 01:29:42 -0300

    proyect timestamp f commit

--- index.js
+++ index.js
@@ -2,31 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
 
 
+app.get('/api/:date?', (req, res) => {
+  let unix = 0;
+  let utc = '';
+
+  if (isNaN(req.params.date)) {
+    const time = new Date(req.params.date)
+    utc = time[Symbol.toPrimitive]('string')
+    unix = time[Symbol.toPrimitive]('number')
+  } else {
+    const time = new Date(`${req.params.date} 00:00:00`)
+    utc = time[Symbol.toPrimitive]('string')
+    unix = time[Symbol.toPrimitive]('number')
+  }
+  res.json({
+    "unix": unix,
+    "utc": utc
+  })
+})
+
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
