9310110520a8012e80f9b0c69fd4865b25ea7c3a
liam <liambialy2@gmail.com>
2024-02-12 19:36:12 -0300

    finished project

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const url = require('node:url')
+const dns = require('node:dns')
+const app = express()
+const bodyParser = require('body-parser')
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(cors())
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use('/public', express.static(`${process.cwd()}/public`))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' })
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+let shorturls = []
+
+const esUrlExistente = (str, callback) => {
+  try {
+    let hostname = new url.URL(str)
+    dns.lookup(hostname.hostname, (err) => {
+      if (err) {
+        callback(false);
+      } else {
+        callback(true);
+      }
+    })
+  } catch (err) {
+    callback(false)
+  }
+}
+
+
+
+app.post('/api/shorturl', (req, res) => {
+  try {
+    let direccionUrl = req.body.url
+    esUrlExistente(direccionUrl, (exis) => {
+      if (exis) {
+        shorturls.push({
+          "original_url": direccionUrl,
+          "short_url": shorturls.length + 1
+        })
+        res.json(shorturls[shorturls.length - 1])
+      } else {
+        res.json({error: 'invalid url'})
+      }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+app.get('/api/shorturl/:id', (req, res) => {
+  esUrlExistente(shorturls[req.params.id - 1].original_url, (existe) => {
+    if (existe) {
+      res.redirect(shorturls[req.params.id - 1].original_url)
+      console.log('accediendo a la url: ' + shorturls[req.params.id - 1].original_url)
+    } else {
+      res.json({error: 'invalid url'})
+    }
+  })
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}`)
+})
